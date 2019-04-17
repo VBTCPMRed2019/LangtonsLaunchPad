a236eb2370ea54f2f99a1c119d39cc22eadc9e5a
VBTCPMRed2019 <46924034+VBTCPMRed2019@users.noreply.github.com>
2019-04-17 13:44:01 -0400

    Update AntMod.py

--- LangtonsAnt/AntMod.py
+++ LangtonsAnt/AntMod.py
@@ -3,7 +3,10 @@ import Images
 
 Images = Images.Images()
 
+
+# Creates the class for the ant
 class Ant:   
+    # Initializes the data for the ant, such as start position and the image it starts on (direction).
     def __init__(self, startpos = {'x' : 0, 'y' : 0}, startframe = Images.Ant[0]):
         self.frame = startframe
         self.curpos = startpos
@@ -12,12 +15,13 @@ class Ant:
         self.prevpos = self.curpos
         self.turns = 0
 
+    # A function that updates the position and manages the previous position of the ant.
     def move(self, direction):
         self.prevpos = self.curpos.copy()
         self.prevdir = self.curdir
         
-        # Direction Fixing WORKS.
-                
+
+        # If the given direction is left, then it'll turn the direction of its ant to the left.        
         if direction == 'l':
             print('turn left')
             if self.prevdir == Images.Ant[3]:
@@ -28,6 +32,8 @@ class Ant:
                 self.curdir = Images.Ant[2]
             elif self.prevdir == Images.Ant[0]:
                 self.curdir = Images.Ant[1]
+                
+        # If the given direction is right, then it'll turn the direction of its ant to the right.
         elif direction == 'r':
             print('turn right')
             if self.prevdir == Images.Ant[3]:
@@ -40,7 +46,7 @@ class Ant:
                 self.curdir = Images.Ant[3]
         self.frame = self.curdir
 
-        # Move The Ant ISSUE SOMEWHERE.
+        # Causes the ant to move forward, if facing forward; left if facing left; right if facing right; down if facing down.
         if self.curdir == Images.Ant[0]:
             print('up')
             if self.curpos['y'] != 448:
@@ -66,7 +72,7 @@ class Ant:
             else:
                 self.curpos['x'] = 0
         print(self.curpos['x'], 'x', self.curpos['y'])
-
+    # Updates the frame of the game to show the ant's new position and the tile's new image.
     def load(self, game, image):
         game.blit(self.frame, (self.curpos['x'], self.curpos['y']))
         if self.turns > 0:
